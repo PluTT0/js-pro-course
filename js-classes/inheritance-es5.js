680dbe33cc80694f9b9810d7272b3b1140b1d1cb
PluTT0 <kos.sok9205@gmail.com>
2021-02-24 18:22:14 +0100

    add subclass

--- js-classes/inheritance-es5.js
+++ js-classes/inheritance-es5.js
@@ -16,12 +16,12 @@ Car.prototype.getFullName = function () {
 Car.prototype.getAge = function () {
   let nowYear = new Date().getFullYear();
   let autoYear = nowYear -= this.year;
-  return console.log( `This car is ${this.year} year,it is ${autoYear} years old`)
+  return console.log( `This car is ${this.year} year,it is ${autoYear} years old`);
 }
 
 Car.prototype.changeColor = function (color) {
   if (this.color === color) {
-    console.log(' This is alredy painted in this color');
+    console.log(' This car is alredy painted in this color');
   } else  {
     this.color = color;
   }
@@ -36,25 +36,79 @@ Car.prototype.calculateWay = function ( kilometers, fuel ) {
     let fuelRemainder = (( kilometers / this.fuelConsumption )-this.fuelCapacity);
 
     if (fuelRemainder > this.fuelCapacity){
-      console.log( `If you want drive ${kilometers}km you must refuel again ${fuelRemainder}l` )
+      console.log( `If you want drive ${kilometers}km you must refuel again ${fuelRemainder}l` );
     } else {
-      console.log( `You can drive ${kilometers}km with out refuel` )
+      console.log( `You can drive ${kilometers}km with out refuel` );
     }
     
   }
 }
 
-const newBmw = new Car('BMW', 'e36', '2005', 'red', 240);
-const newFord = new Car('Ford', 'Mustag', '2020', 'blue', 320);
-const newHonda = new Car('Honda', 'Accord', '2015', 'grey', 280);
+//Subclass BMW
+
+function Bmw (name, model, year, color, maxSpeed, fuelCapacity, fuelConsumption, option) {
+  Car.call(this, name, model, year, color, maxSpeed, fuelCapacity, fuelConsumption);
+  this.option = 'standart';
+}
+
+Bmw.prototype = Object.create(Car.prototype);
+Bmw.prototype.constructor = Bmw;
+
+Bmw.prototype.superOption = function () {
+  if (this.model === 'e36', 'm4', 'x5') {
+    this.option = 'cabriolet';
+  }
+}
+
+//Subclass Ford
+
+function Ford (name, model, year, color, maxSpeed, fuelCapacity, fuelConsumption, motor,option) {
+  Car.call(this, name, model, year, color, maxSpeed, fuelCapacity, fuelConsumption);
+  this.option = 'standart';
+}
+
+Ford.prototype = Object.create(Car.prototype);
+Ford.prototype.constructor = Ford;
+
+Ford.prototype.superOption = function (option) {
+  if (this.model === 'Mustang', 'mustang') {
+    this.motor = 'v8, power 500';
+    this.option = 'led light'
+  }
+}
+
+//Subclass Honda
+
+function Honda (name, model, year, color, maxSpeed, fuelCapacity, fuelConsumption, option) {
+  Car.call(this, name, model, year, color, maxSpeed, fuelCapacity, fuelConsumption);
+  this.option = 'standart';
+}
+
+Honda.prototype = Object.create(Car.prototype);
+Honda.prototype.constructor = Honda;
 
 
+Honda.prototype.superOption = function () {
+  if (this.model === 'Accord', 'accord') {
+    this.option = 'Panoramic roof';
+  }
+}
+
+
+
+const newBmw = new Bmw('BMW', 'e36', '2005', 'red', 240);
+const newFord = new Ford('Ford', 'Mustag', '2020', 'blue', 320);
+const newHonda = new Honda('Honda', 'Accord', '2015', 'grey', 280);
+const newCivic = new Honda('Honda', 'Civic', '2004', 'blue', 200);
+
+newBmw.superOption();
+newFord.superOption();
+newHonda.superOption();
 newFord.changeColor('green');
-newBmw.calculateWay(6900 ,20)
-
+newBmw.calculateWay(6900 ,20);
+newHonda.changeColor('grey');
 
 console.log(newBmw);
 console.log(newFord);
 console.log(newHonda);
-
-newBmw.changeColor('red')
+console.log(newCivic);
