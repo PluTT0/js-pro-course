c275e8acc69339cf10e4e63a48177ee5c9b51627
PluTT0 <kos.sok9205@gmail.com>
2021-03-15 04:00:45 +0100

    add component(npm)

--- notification-component/src/getData.js
+++ notification-component/src/getData.js
@@ -1,11 +1,13 @@
+const lodash = require('lodash');
+
 import { showNotification } from './script.js'
 
 
 async function getData() {
-  const res = await fetch('./data.json')
+  const res = await fetch('./data.json');
   const data  = await res.json();
   showNotification(data);
-  console.log(data)
+  console.log(data);
 }
 
 getData()
